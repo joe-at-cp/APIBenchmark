def3ad55657ae255c76684f8755ee0f8abd9d7ab
joe-at-cp <dilligj@checkpoint.com>
2019-01-28 14:59:14 -0600

    deleted:    .benchmark_delete 	modified:   APIBenchmark.py

--- APIBenchmark.py
+++ APIBenchmark.py
@@ -38,17 +38,35 @@ d=1
 cp_url='https://'+cp_server+'/web_api'
 cp_sid=''
 total_time=0
+duplicate_objects=0
+unique_objects=0
 
 print '[+] Check Point Management API Benchmark Tool '+version
 print '[+] Joe Dillig - Check Point Software 2018'
 
 
 def AddHost(host_name,host_ip):
+    global unique_objects
+    global duplicate_objects
+
     url=cp_url+'/add-host'
     headers = {'Content-type': 'application/json', 'Accept': 'bla', 'X-chkp-sid': cp_sid}
     data = {'name':host_name, 'ip-address':host_ip, 'comments':'API Benchmark Host','color':'orange', 'set-if-exists':True}
     r = requests.post(url, data=json.dumps(data), headers=headers, verify=False)
-    #jsonreturn = json.loads(r.text)
+    hostreturn = json.loads(r.text)
+
+    #print(hostreturn['meta-info']['lock'])
+    #Object Validation (Did the object already exist?)
+    try:
+        if hostreturn['meta-info']['lock'] == 'locked by current session':
+            duplicate_objects = duplicate_objects + 1
+            #print(' -> Duplicate Host Exists!')
+        else:
+            unique_objects=unique_objects+1
+            #print(' -> Unique Host Exists!')
+    except:
+        print('failed to get return data')
+
     #print(jsonreturn)
 
 def DeleteHost(host_name):
@@ -57,174 +75,216 @@ def DeleteHost(host_name):
     data = {'name':host_name}
     r = requests.post(url, data=json.dumps(data), headers=headers, verify=False)
 
+try:
+    #Login
+    url=cp_url+'/login'
+    headers = {'Content-type': 'application/json', 'Accept': 'bla'}
+    data = {'user': cp_user, 'password': cp_pass, 'session-timeout': '3600', 'session-name': 'Management API Benchmark Tool'}
+    r = requests.post(url, data=json.dumps(data), headers=headers, verify=False)
+    jsonreturn = json.loads(r.text)
+    #print(jsonreturn)
+    cp_sid = jsonreturn['sid']
 
-#Login
-url=cp_url+'/login'
-headers = {'Content-type': 'application/json', 'Accept': 'bla'}
-data = {'user': cp_user, 'password': cp_pass, 'session-timeout': '3600', 'session-name': 'Management API Benchmark Tool'}
-r = requests.post(url, data=json.dumps(data), headers=headers, verify=False)
-jsonreturn = json.loads(r.text)
-#print(jsonreturn)
-cp_sid = jsonreturn['sid']
+    count=0
 
-count=0
+    if benchmark_mode == 'count':
 
-if benchmark_mode == 'count':
+        #Write Delete File Before Starting Benchmark
+        f = open(".benchmark_delete", "w")
+        f.write(benchmark_hosts)
 
-    #Write Delete File Before Starting Benchmark
-    f = open(".benchmark_delete", "w")
-    f.write(benchmark_hosts)
+        print '[+] Starting Benchmark (Count Mode)'
+        print '    Objects: '+str(benchmark_hosts)
 
-    print '[+] Starting Benchmark (Count Mode)'
-    print '[-]   Objects: '+str(benchmark_hosts)
+        progress=0
+        total_time=0
+        progress_increment = (100 / int(benchmark_hosts))
+        obj_per_sec_count=0
+        obj_per_sec_timer=0
+        obj_per_sec_print=0
+        stdscr = curses.initscr()
+        while int(count) < int(benchmark_hosts):
 
-    progress=0
-    total_time=0
-    progress_increment = (100 / int(benchmark_hosts))
-    obj_per_sec_count=0
-    obj_per_sec_timer=0
-    obj_per_sec_print=0
-    stdscr = curses.initscr()
-    while int(count) < int(benchmark_hosts):
+            host_name='BenchmarkHost'+str(count)
 
-        host_name='BenchmarkHost'+str(count)
+            if d < 255:
+                d=d+1
+                host_ip = str(a)+"."+str(b)+"."+str(c)+"."+str(d)
 
-        if d < 255:
-            d=d+1
-            host_ip = str(a)+"."+str(b)+"."+str(c)+"."+str(d)
+                #Start Timers
+                create_start = time.time() #Timer
 
-            #Start Timers
-            create_start = time.time() #Timer
+                #Create Object
+                AddHost(host_name,host_ip)
 
-            #Create Object
-            AddHost(host_name,host_ip)
+                #End Timers
+                create_finish = time.time() #Timer finish
 
-            #End Timers
-            create_finish = time.time() #Timer finish
+                #Calculate Time
+                elapsed = create_finish - create_start
+                total_time = total_time + elapsed
 
-            #Calculate Time
-            elapsed = create_finish - create_start
-            total_time = total_time + elapsed
+                obj_per_sec_timer = obj_per_sec_timer + elapsed
+                if obj_per_sec_timer >= 1:
+                    #Log and Reset Time Count
+                    obj_per_sec_print = obj_per_sec_count
+                    obj_per_sec_timer=0
+                    obj_per_sec_count=0
 
-            obj_per_sec_timer = obj_per_sec_timer + elapsed
-            if obj_per_sec_timer >= 1:
-                #Log and Reset Time Count
-                obj_per_sec_print = obj_per_sec_count
-                obj_per_sec_timer=0
-                obj_per_sec_count=0
+                running_avg = count/float(total_time)
+                count=count+1
+                obj_per_sec_count=obj_per_sec_count+1
 
-            running_avg = count/float(total_time)
+                progress = float(count) / float(benchmark_hosts) * 100
+
+                #Progress Box
+                stdscr.addstr(0, 0, "[+] Benchmark Running - Count Mode - "+str(benchmark_hosts)+' Objects')
+                stdscr.addstr(1, 0, "    Objects Created: "+str(count))
+                stdscr.addstr(2, 0, "    Progress: %.2f" % round(progress,2)+"%")
+                stdscr.addstr(3, 0, "    Avg Time Per Object: %.2f" % round(elapsed,2)+" Seconds")
+                stdscr.addstr(4, 0, "    Objects Per Second: "+str(obj_per_sec_print))
+                stdscr.addstr(5, 0, "    Elapsed Time: %.0f" % round(total_time)+" Seconds")
+                stdscr.addstr(6, 0, "    Unique Objects: "+str(unique_objects))
+                stdscr.addstr(7, 0, "    Duplicate Objects: "+str(duplicate_objects))
+                stdscr.refresh()
+
+            elif c < 255:
+                c=c+1
+                d=1 #Reset D
+
+            elif b < 255:
+                b=b+1
+                c=1 #reset c
+                d=1 #reset d
+
+            elif a < 255:
+                a=a+1
+                b=1 #reset b
+                c=1 #reset c
+                d=1 #reset d
+
+    if benchmark_mode == 'time':
+
+        print '[+] Starting Benchmark - Time Mode'
+        print '    Time: '+str(benchmark_time)+' Seconds'
+
+        total_time=0
+        count=0
+        obj_per_sec_count=0
+        obj_per_sec_timer=0
+        obj_per_sec_print=0
+
+        stdscr = curses.initscr()
+        time_mode_start = time.time()
+
+        while True:
+
+            host_name='BenchmarkHost'+str(count)
+
+            if d < 255:
+                d=d+1
+                host_ip = str(a)+"."+str(b)+"."+str(c)+"."+str(d)
+
+                create_start = time.time() #Timer
+                AddHost(host_name,host_ip)
+                create_finish = time.time() #Timer finish
+
+                elapsed = create_finish - create_start
+                total_time = total_time + elapsed
+
+                if float(total_time) >= float(benchmark_time):
+                    benchmark_hosts = count
+                    break
+
+                obj_per_sec_timer = obj_per_sec_timer + elapsed
+                if obj_per_sec_timer >= 1:
+                    #Log and Reset Time Count
+                    obj_per_sec_print = obj_per_sec_count
+                    obj_per_sec_timer=0
+                    obj_per_sec_count=0
+
+                #Progress Box
+                stdscr.addstr(0, 0, "[+] Benchmark Running - Time Mode - "+str(benchmark_time)+' Seconds')
+                stdscr.addstr(1, 0, "    Objects Created: "+str(count))
+                stdscr.addstr(2, 0, "    Avg Time Per Object: %.2f" % round(elapsed,2)+" Seconds")
+                stdscr.addstr(3, 0, "    Objects Per Second: "+str(obj_per_sec_print))
+                stdscr.addstr(4, 0, "    Elapsed Time: %.0f" % round(total_time)+" Seconds")
+                stdscr.addstr(5, 0, "    Unique Objects: "+str(unique_objects))
+                stdscr.addstr(6, 0, "    Duplicate Objects: "+str(duplicate_objects))
+                stdscr.refresh()
+
+                count=count+1
+                obj_per_sec_count=obj_per_sec_count+1
+
+            elif c < 255:
+                c=c+1
+                d=1 #Reset D
+
+            elif b < 255:
+                b=b+1
+                c=1 #reset c
+                d=1 #reset d
+
+            elif a < 255:
+                a=a+1
+                b=1 #reset b
+                c=1 #reset c
+                d=1 #reset d
+
+            #Write delete file
+            f = open(".benchmark_delete", "w")
+            f.write(str(count))
+
+
+    if benchmark_mode == 'delete':
+
+        #Read .benchmark_delete file
+        f = open(".benchmark_delete", "r")
+        benchmark_hosts= f.read()
+
+        obj_per_sec_count=0
+        obj_per_sec_timer=0
+        obj_per_sec_print=0
+        stdscr = curses.initscr()
+        print '[+] Deleting '+benchmark_hosts+' Benchmark Objects...'
+
+        while int(count) < int(benchmark_hosts):
+
+            host_name='BenchmarkHost'+str(count)
+            DeleteHost(host_name)
+            progress=count/100
             count=count+1
-            obj_per_sec_count=obj_per_sec_count+1
 
             progress = float(count) / float(benchmark_hosts) * 100
 
             #Progress Box
-            stdscr.addstr(0, 0, "[+] Benchmark Running...")
-            stdscr.addstr(1, 0, "    Objects Created: "+str(count))
+            stdscr.addstr(0, 0, "[+] Deleting Benchmark Objects")
+            stdscr.addstr(1, 0, "    Objects Deleted: "+str(count))
             stdscr.addstr(2, 0, "    Progress: %.2f" % round(progress,2)+"%")
-            stdscr.addstr(3, 0, "    Avg Time Per Object: %.2f" % round(elapsed,2)+" Seconds")
-            stdscr.addstr(4, 0, "    Objects Per Second: "+str(obj_per_sec_print))
-            stdscr.addstr(5, 0, "    Elapsed Time: %.0f" % round(total_time)+" Seconds")
             stdscr.refresh()
 
-        elif c < 255:
-            c=c+1
-            d=1 #Reset D
 
-        elif b < 255:
-            b=b+1
-            c=1 #reset c
-            d=1 #reset d
+        os.remove(".benchmark_delete")
 
-        elif a < 255:
-            a=a+1
-            b=1 #reset b
-            c=1 #reset c
-            d=1 #reset d
-
-
-if benchmark_mode == 'time':
-
-    print '[+] Starting Benchmark (Time Mode)'
-
-    total_time=0
-    time_mode_start = time.time()
-
-    while count < benchmark_hosts:
-
-        host_name='BenchmarkHost'+str(count)
-
-        if d < 255:
-            d=d+1
-            host_ip = str(a)+"."+str(b)+"."+str(c)+"."+str(d)
-
-            create_start = time.time() #Timer
-            AddHost(host_name,host_ip)
-
-            create_finish = time.time() #Timer finish
-            elapsed = create_finish - create_start
-            total_time = total_time + elapsed
-
-            if total_time >= benchmark_time:
-                break
-
-            count=count+1
-
-            sys.stdout.write('[+] Progress: ?' )
-            sys.stdout.write('    Objects Created: '+str(count)+'/'+str(benchmark_hosts))
-            sys.stdout.write('    Elapsed Time:'+total_time)
-            sys.stdout.flush()
-
-
-        elif c < 255:
-            c=c+1
-            d=1 #Reset D
-
-        elif b < 255:
-            b=b+1
-            c=1 #reset c
-            d=1 #reset d
-
-        elif a < 255:
-            a=a+1
-            b=1 #reset b
-            c=1 #reset c
-            d=1 #reset d
-
-
-if benchmark_mode == 'delete':
-
-    #Read .benchmark_delete file
-    f = open(".benchmark_delete", "r")
-    benchmark_hosts= f.read()
-    stdscr = curses.initscr()
-    print '[+] Deleting '+benchmark_hosts+' Benchmark Objects...'
-
-    while int(count) < int(benchmark_hosts):
-
-        host_name='BenchmarkHost'+str(count)
-        DeleteHost(host_name)
-        progress=count/100
-        count=count+1
-
-        progress = float(count) / float(benchmark_hosts) * 100
-
-        #Progress Box
-        stdscr.addstr(0, 0, "[+] Deleting Benchmark Objects")
-        stdscr.addstr(1, 0, "    Objects Deleted: "+str(count))
-        stdscr.addstr(2, 0, "    Progress: %.2f" % round(progress,2)+"%")
-        stdscr.refresh()
-
-
-    os.remove(".benchmark_delete")
-curses.endwin()
+except Exception as e:
+    print('exception occured: {}'.format(e))
+finally:
+    curses.endwin()
 
 avg_time=float(total_time) / float(benchmark_hosts)
 
-print '[+] Total Time: '+str(round(total_time))+' seconds'
-print '[+] Objects Per Second: ~'+str(obj_per_sec_print)
-print '[+] Average Time Per Host Object: '+str(avg_time)
+print '[+] ########## Benchmark Results ##########'
+print '[+] Objects Statistics'
+print '    Created Objects: '+str(benchmark_hosts)
+print '    Unique Objects: '+str(unique_objects)
+print '    Duplicate Objects: '+str(duplicate_objects)
+
+print '[+] Time Statistics'
+print '    Total Run Time: '+str(round(total_time))+' seconds'
+print '    Objects Per Second: '+str(obj_per_sec_print)
+print '    Average Creation Time Per Object: %.2f' % round(avg_time,2)+' Seconds'
+
+print '[+] Publish Statistics'
 
 
 #Publish Session
@@ -241,17 +301,14 @@ while True:
     data = {'task-id':jsonreturn['task-id']}
     r = requests.post(url, data=json.dumps(data), headers=headers, verify=False)
     taskcheck = json.loads(r.text)
-    #print('Checking Publish Task: '+str(taskcheck['tasks'][0]['status'])+' Progress: '+str(taskcheck['tasks'][0]['progress-percentage']))
-    #print('Checking Publish Task: '+taskcheck['tasks'][0])
 
     if str(taskcheck['tasks'][0]['status']) == 'succeeded':
-        #print('task status='+str(taskcheck['tasks'][0]['status'])+'     Exiting loop')
-        #print(str(taskcheck['tasks'][0]['status']))
         break
 
 publish_finish = time.time()
 publish_elapsed = publish_finish - publish_start
-print '[+] Publishing Time: '+str(publish_elapsed)
+
+print '    Publishing Time: %.2f' % round(publish_elapsed,2)+' Seconds'
 
 
 #Logout
@@ -259,5 +316,5 @@ url=cp_url+'/logout'
 headers = {'Content-type': 'application/json', 'Accept': 'bla', 'X-chkp-sid': cp_sid}
 r = requests.post(url, data='{}', headers=headers, verify=False)
 jsonreturn = json.loads(r.text)
-print '[+] Logout - '+jsonreturn['message']
+print '    Logout - '+jsonreturn['message']
 
